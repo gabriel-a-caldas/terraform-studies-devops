c7a3377d4978a9fdac2fd82ca6ffbb0ca174035a
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-05-05 17:44:30 -0300

    Using input variables

---
+++ 8.Variables/variables_example_01/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+variable "bucket_name" {
+  description = "the name of the bucket you wish to create"
+}
+
+resource "aws_s3_bucket" "generic_bucket" {
+  bucket = var.bucket_name
+}
