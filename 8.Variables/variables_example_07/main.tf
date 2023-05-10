63c6e247acce0b14ba14a4c81c685eb8ae3e8fe4
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-05-10 17:25:55 -0300

    Understanding the use of sets

---
+++ 8.Variables/variables_example_07/main.tf
@@ -0,0 +1,24 @@
+# COMPLEX TYPE: set()
+# CONTEXT: List with only unique values.
+
+variable "my_set" {
+  type = set(number)
+  default = [ 7, 2, 2 ]
+}
+
+variable "my_list" {
+  type = list(string)
+  default = [ "foo", "bar", "foo" ]
+}
+
+output "set" {
+  value = var.my_set
+}
+
+output "list" {
+  value = var.my_list
+}
+
+output "list_as_set" {
+  value = toset(var.my_list)
+}
