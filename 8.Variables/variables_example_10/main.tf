208e029d6f0d2f544cac8a7876c16d27bcf65b91
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-05-11 17:25:15 -0300

    Using the object() structure

---
+++ 8.Variables/variables_example_10/main.tf
@@ -0,0 +1,44 @@
+# COMPLEX TYPE: object()
+# CONTEXT: Structure type that allows you to define complex objects and constrain types to them
+
+variable "person" {
+  type = object({
+    name = string,
+    age  = number
+  })
+  default = {
+    name = "Bob"
+    age  = 10
+  }
+}
+
+output "person" {
+  value = var.person
+}
+
+variable "person_with_address" {
+  type = object({
+    name = string,
+    age  = number,
+    address = object({
+      line1    = string,
+      line2    = string,
+      county   = string,
+      postcode = string
+    })
+  })
+  default = {
+    name = "Jim"
+    age  = 21
+    address = {
+      line1    = "1 the road"
+      line2    = "St Ives"
+      county   = "Cambridgeshire"
+      postcode = "CB1 2GB"
+    }
+  }
+}
+
+output "person_with_address" {
+  value = var.person_with_address
+}
